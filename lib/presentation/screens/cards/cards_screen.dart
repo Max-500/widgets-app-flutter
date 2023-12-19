05c6a9a3081ed866cdb410b9dd228e6bb45d3ca1
Max-500 <213497@ids.upchiapas.edu.mx>
2023-12-18 22:22:45 -0600

    Basic Cards

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+
+const cards = <Map<String, dynamic>> [
+  {'elevation': 0.0, 'label': 'Elevation 0'},
+  {'elevation': 1.0, 'label': 'Elevation 1'},
+  {'elevation': 2.0, 'label': 'Elevation 2'},
+  {'elevation': 3.0, 'label': 'Elevation 3'},
+  {'elevation': 4.0, 'label': 'Elevation 4'},
+  {'elevation': 5.0, 'label': 'Elevation 5'},
+];
 
 class CardsScreen extends StatelessWidget {
   static const String name = 'cards_screen';
@@ -22,6 +30,44 @@ class _CardsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ...cards.map(
+            (card) => _CardTypeOne(label: card['label'], elevation: card['elevation'])
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _CardTypeOne extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardTypeOne({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: elevation,
+      child: Padding( 
+        padding: const EdgeInsets.fromLTRB(10, 5, 5, 10),
+        child: Column( 
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(onPressed: (){}, icon: const Icon( Icons.more_vert_outlined ))
+            ),
+
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(label),
+            )
+          ]
+        ),
+      ),
+    );
   }
 }
