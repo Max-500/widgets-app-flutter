6ddaeec8fa343f2f303becda8de62705777b914c
Max-500 <213497@ids.upchiapas.edu.mx>
2023-12-18 21:38:20 -0600

    Buttons Material 3

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CardsScreen extends StatelessWidget {
   static const String name = 'cards_screen';
@@ -8,10 +9,53 @@ class CardsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Cards Screen"),
+        appBar: AppBar(
+          title: const Text("Cards Screen"),
+        ),
+        body: _ButtonsView(),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.arrow_back_rounded),
+          onPressed: () {
+            context.pop();
+          },
+        ));
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme colors = Theme.of(context).colorScheme;
+
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+        child: Wrap(
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () {}, child: const Text('Elevated Button')),
+            const ElevatedButton(onPressed: null, child: Text('Elevated Button Disabled')),
+            ElevatedButton.icon(onPressed: (){  }, icon: const Icon( Icons.access_alarms_rounded ), label: const Text('Elevated Button Icon')),
+
+            FilledButton(onPressed: () {  }, child: const Text('Filled Button')),
+            FilledButton.icon(onPressed: null, icon: const Icon( Icons.accessibility_rounded ), label: const Text('Filled Button Icon')),
+
+            OutlinedButton(onPressed: () { }, child: const Text('Outlined Button')),
+            OutlinedButton.icon(onPressed: () { }, icon: const Icon( Icons.terminal_rounded ), label: const Text('Outlined Button Icon')),
+
+            TextButton(onPressed: (){}, child: const Text('Text Button')),
+            TextButton.icon(onPressed: (){}, icon: const Icon( Icons.terminal_sharp ), label: const Text('Text Button Icon')),
+
+            IconButton(onPressed: (){}, icon: const Icon( Icons.app_registration_rounded )),
+            IconButton(onPressed: (){}, icon: const Icon( Icons.app_registration_rounded ), style: ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll(colors.primary),
+              iconColor: const MaterialStatePropertyAll(Colors.white)
+            ),),
+          ],
+        ),
       ),
-      body: const Placeholder(),
     );
   }
 }
