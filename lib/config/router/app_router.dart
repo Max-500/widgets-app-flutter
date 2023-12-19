f621ca9879532127576e6858623e419b2025075b
Max-500 <213497@ids.upchiapas.edu.mx>
2023-12-19 11:50:51 -0600

    Animated Container Finished

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -3,26 +3,11 @@ import 'package:widgets_app/presentation/screens/screens.dart';
 
 final appRouter = GoRouter(
   routes: [
-    GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
-    ),
-    GoRoute(
-      path: '/cards',
-      name: CardsScreen.name,
-      builder: (context, state) => const CardsScreen(),
-    ),
-    GoRoute(
-      path: '/buttons',
-      name: ButtonsScreen.name,
-      builder: (context, state) => const ButtonsScreen(),
-    ),
-    GoRoute(
-      path: '/progress',
-      name: ProgressScreen.name,
-      builder: (context, state) => const ProgressScreen(),
-    ),
-    GoRoute(path: '/snackbars', name: SnackBarScreen.name, builder: (context, state) => const SnackBarScreen(),),
+    GoRoute(path: '/', name: HomeScreen.name, builder: (context, state) => const HomeScreen()),
+    GoRoute(path: '/cards', name: CardsScreen.name, builder: (context, state) => const CardsScreen()),
+    GoRoute(path: '/buttons', name: ButtonsScreen.name, builder: (context, state) => const ButtonsScreen()),
+    GoRoute(path: '/progress', name: ProgressScreen.name, builder: (context, state) => const ProgressScreen()),
+    GoRoute(path: '/snackbars', name: SnackBarScreen.name, builder: (context, state) => const SnackBarScreen()),
+    GoRoute(path: '/animated', name: AnimatedScreen.name, builder: (context, state) => const AnimatedScreen())
   ],
 );
