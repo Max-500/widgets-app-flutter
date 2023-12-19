b7976dd2775a133b64dedbb04e0192fb10d2919a
Max-500 <213497@ids.upchiapas.edu.mx>
2023-12-18 23:05:25 -0600

    Screens created and updated

---
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProgressScreen extends StatelessWidget {
+  static const String name = 'progress_scren';
+
+  const ProgressScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
