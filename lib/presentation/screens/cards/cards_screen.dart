6bef59308687dea292be8410f227fcc053553ca5
Max-500 <213497@ids.upchiapas.edu.mx>
2023-12-18 22:01:05 -0600

    Fixin orden

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -12,102 +12,16 @@ class CardsScreen extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Cards Screen"),
         ),
-        body: _ButtonsView(),
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.arrow_back_rounded),
-          onPressed: () {
-            context.pop();
-          },
-        ));
-  }
-}
-
-class _ButtonsView extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final ColorScheme colors = Theme.of(context).colorScheme;
-
-    return SizedBox(
-      width: double.infinity,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-        child: Wrap(
-          spacing: 10,
-          alignment: WrapAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: () {}, child: const Text('Elevated Button')),
-            const ElevatedButton(
-                onPressed: null, child: Text('Elevated Button Disabled')),
-            ElevatedButton.icon(
-                onPressed: () {},
-                icon: const Icon(Icons.access_alarms_rounded),
-                label: const Text('Elevated Button Icon')),
-            FilledButton(onPressed: () {}, child: const Text('Filled Button')),
-            FilledButton.icon(
-                onPressed: null,
-                icon: const Icon(Icons.accessibility_rounded),
-                label: const Text('Filled Button Icon')),
-            OutlinedButton(
-                onPressed: () {}, child: const Text('Outlined Button')),
-            OutlinedButton.icon(
-                onPressed: () {},
-                icon: const Icon(Icons.terminal_rounded),
-                label: const Text('Outlined Button Icon')),
-            TextButton(onPressed: () {}, child: const Text('Text Button')),
-            TextButton.icon(
-                onPressed: () {},
-                icon: const Icon(Icons.terminal_sharp),
-                label: const Text('Text Button Icon')),
-            IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.app_registration_rounded)),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.app_registration_rounded),
-              style: ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll(colors.primary),
-                  iconColor: const MaterialStatePropertyAll(Colors.white)),
-            ),
-            const CustomButton()
-          ],
-        ),
-      ),
+        body: const _CardsView(),
     );
   }
 }
 
-class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
+class _CardsView extends StatelessWidget {
+  const _CardsView();
 
   @override
   Widget build(BuildContext context) {
-    final ColorScheme colors = Theme.of(context).colorScheme;
-
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(20),
-      child: Material(
-        color: colors.primary,
-        child: InkWell(
-          onTap: () {},
-          child: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Colors.red,
-                  Colors.blue
-                ], // Reemplaza estos colores con los de tu gradiente
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-            ),
-            child: const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: Text('Hola mundo'),
-            ),
-          ),
-        ),
-      ),
-    );
+    return Container();
   }
 }
