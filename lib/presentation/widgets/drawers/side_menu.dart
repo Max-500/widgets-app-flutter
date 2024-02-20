f2152ea21ceaf002755951a69d1746d5e1cbce0e
Max-500 <213497@ids.upchiapas.edu.mx>
2024-02-19 22:12:59 -0600

    Navigation Drawer Better

--- lib/presentation/widgets/drawers/side_menu.dart
+++ lib/presentation/widgets/drawers/side_menu.dart
@@ -1,4 +1,7 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu-items/menu_items.dart';
 
 class SideMenu extends StatefulWidget {
   const SideMenu({super.key});
@@ -12,15 +15,34 @@ class _SideMenuState extends State<SideMenu> {
 
   @override
   Widget build(BuildContext context) {
+    final bool hasNotch = MediaQuery.of(context).viewPadding.top > 35;
+
     return NavigationDrawer(
-      selectedIndex: navDrawerIndex + 1,
+      selectedIndex: navDrawerIndex,
       onDestinationSelected: (value) {
         navDrawerIndex = value;
         setState(() {});
       },
-      children: const [
-        NavigationDrawerDestination(icon: Icon(Icons.add), label: Text('Home Screen')),
-        NavigationDrawerDestination(icon: Icon(Icons.add), label: Text('Home Screen'))
+      children: [
+        Padding(
+          padding: EdgeInsets.fromLTRB(30, hasNotch ? 10 : 20, 20, 10),
+          child: const Text('Main Options')
+          ),
+
+        ...menuItems
+          .sublist(0, 4)
+          .map((item) => NavigationDrawerDestination(icon: Icon(item.icon), label: Text(item.title))),
+
+          const Padding(padding: EdgeInsets.fromLTRB(25, 30, 25, 30), child: Divider(),),
+          const Padding(
+          padding: EdgeInsets.all(5),
+          child: Text('More Options')
+          ),
+
+
+          ...menuItems
+          .sublist(4, menuItems.length)
+          .map((item) => NavigationDrawerDestination(icon: Icon(item.icon), label: Text(item.title), )),
       ],
     );
   }
