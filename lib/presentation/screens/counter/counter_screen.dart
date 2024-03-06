e01d34ba56a07fa9c160dd4de82a8d714bdae323
Max-500 <213497@ids.upchiapas.edu.mx>
2024-03-06 12:30:03 -0600

    antes de notifier y agregando heroTag

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -12,25 +12,44 @@ class CounterScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final int clickCounter = ref.watch( counterProvider );
     final bool isDarkMode = ref.watch( isDarkModeProvider );
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeigth = MediaQuery.of(context).size.height;
+
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Counter Screen with Riverpod'),
-        actions: [
-          IconButton(
-            onPressed: () => ref.read(isDarkModeProvider.notifier).state = !isDarkMode, 
-            icon: Icon( isDarkMode ? Icons.dark_mode : Icons.light_mode )
-          )
-        ],
+        title: const Text('Counter Screen with Riverpod')
       ),
       body: Center(
-        child: Text('Valor: $clickCounter', style: Theme.of(context).textTheme.titleLarge,)
-        ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          ref.read(counterProvider.notifier).state++;
-        } ,
-        child: const Icon(Icons.add),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Valor: $clickCounter', style: Theme.of(context).textTheme.titleLarge,),
+            SizedBox(height: screenHeigth*0.10,),
+             Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  FloatingActionButton(
+                    heroTag: 'btn to plus one',
+                    onPressed: () => ref.read(counterProvider.notifier).state++,
+                    child: const Icon(Icons.add),
+                  ),
+                  FloatingActionButton(
+                    heroTag: 'btn to reset to zero',
+                    onPressed: () => ref.read(counterProvider.notifier).state = 0,
+                    child: const Icon(Icons.refresh_outlined),
+                  ),
+                  FloatingActionButton(
+                    heroTag: 'btn to minus one',
+                    onPressed: () => ref.read(counterProvider.notifier).state--,
+                    child: const Text('-', style: TextStyle(
+                      fontSize: 20
+                    ),),
+                  ),
+                ],
+              ),
+            ]
+          ),
       ),
     );
   }
